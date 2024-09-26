513cb80470641d4b0aac0b578f51cb40fe9050d8
KloseBH <danielhabdias@gmail.com>
2024-09-26 10:13:58 -0300

    Organização

--- AppWeb/Utilizando .csv/AddTarefa.php
+++ AppWeb/Utilizando .csv/AddTarefa.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $titulo = $_POST['titulo'];
@@ -33,39 +32,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit;
 }
 ?>
-=======
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $titulo = $_POST['titulo'];
-    $mensagem = $_POST['mensagem'];
-    $prioridade = $_POST['prioridade'];
-    $situacao = "aberto";
-
-    $arquivo = 'tarefas.csv';
-
-    $handle = fopen($arquivo, 'a+');
-
-    if ($handle === false) {
-        die("Erro ao abrir o arquivo CSV para escrita.");
-    }
-
-    $id = 1;
-    $linhas = file($arquivo);
-    if (!empty($linhas)) {
-        $ultimaLinha = end($linhas);
-        $dadosUltimaLinha = str_getcsv($ultimaLinha);
-        $id = intval($dadosUltimaLinha[0]) + 1;
-    }
-
-    $novaLinha = [$id, $titulo, $mensagem, $prioridade, $situacao];
-
-    fputcsv($handle, $novaLinha);
-
-    fclose($handle);
-
-    echo "Tarefa adicionada com sucesso! ID: $id";
-    header('Location: Tarefas.html');
-    exit;
-}
-?>
->>>>>>> 372a888615a22f72b88a2f8ee25eafe1192da021
--- AppWeb/Utilizando .csv/CarregarTarefas.php
+++ AppWeb/Utilizando .csv/CarregarTarefas.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $arquivo_csv = 'tarefas.csv';
 
@@ -24,30 +23,3 @@ if ($arquivo !== false) {
     echo "Não foi possível abrir o arquivo CSV para leitura.";
 }
 ?>
-=======
-<?php
-$arquivo_csv = 'tarefas.csv';
-
-if (!file_exists($arquivo_csv)) {
-    die("Arquivo não encontrado: $arquivo_csv");
-}
-
-$arquivo = fopen($arquivo_csv, 'r');
-
-if ($arquivo !== false) {
-    $dados = [];
-
-    while (($linha = fgetcsv($arquivo)) !== false) {
-        $dados[] = $linha;
-    }
-
-    fclose($arquivo);
-
-    $dados_json = json_encode($dados);
-
-    echo $dados_json;
-} else {
-    echo "Não foi possível abrir o arquivo CSV para leitura.";
-}
-?>
->>>>>>> 372a888615a22f72b88a2f8ee25eafe1192da021
